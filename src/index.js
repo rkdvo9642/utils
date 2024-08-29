63b385d6863ffd353ba5eb727297d5ee4584fbc5
hyeok <rkdvo9642@naver.com>
2024-08-29 10:00:49 +0900

    gh-pages

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Router from './router';
+import Router from "./router";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Router />);
+root.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Router />
+  </BrowserRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
