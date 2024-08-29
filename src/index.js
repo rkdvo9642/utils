242d139b6d56108bedc660419a457f314218741d
hyeok <rkdvo9642@naver.com>
2024-08-29 10:09:30 +0900

    env 환경변수 이름 변경

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
     <Router />
   </BrowserRouter>
 );
